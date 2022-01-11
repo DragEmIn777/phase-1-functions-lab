581a470216f051ff4a3cf8fe46a6262d1757a5d2
Matt Draghi <mtd5264@gmail.com>
2022-01-11 12:06:57 -0500

    commit tree 80b6cc0a256be68a341a9fc640921023c535c3e2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+let hq = 42;
+
+function distanceFromHqInBlocks(pickupLoc){
+    return Math.max(hq, pickupLoc) - Math.min(hq, pickupLoc);
+}
+
+function distanceFromHqInFeet(pickupLoc){
+    return distanceFromHqInBlocks(pickupLoc) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.max(start, destination) - Math.min(start, destination)) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let fee;
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        distance -= 400;
+        fee = 0;
+        return fee;
+    }
+    else if (distance > 400 && distance < 2000){
+        distance -= 400;
+        fee = distance * 0.02;
+        return fee;
+    }
+    else if (distance >= 2000 && distance <= 2500){
+        fee = 25;
+        return fee;
+    }
+    else if (distance > 2500){
+        return `cannot travel that far`;
+    }
+}
